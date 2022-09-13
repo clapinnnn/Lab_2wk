501ea6fe72f9a2162bd87f3b5f892a6d88cf4818
수인 박 <psi7599@naver.com>
2022-09-14 00:31:08 +0900

    코드 수정

--- Lab_2wk/Q1.cpp
+++ Lab_2wk/Q1.cpp
@@ -6,15 +6,21 @@ int main() {
 	char ch;
 
 	cout << "영문자를 입력하고 ctrl+z를 치세요" << endl;
+
 	while (cin >> ch) {
 		switch (ch) {
 		case 'a': case 'i': case 'e': case 'o': case 'u':
 			vowel++; break;
-		default:
+		case 'b': case 'c': case 'd': case 'f': case 'g':
+		case 'h': case 'j': case 'k': case 'l': case 'm':
+		case 'n': case 'p': case 'q': case 'r': case 's':
+		case 't': case 'u': case 'v': case 'w': case 'x':
+		case 'y': case 'z': 
 			consonant++; break;
+		default:
+			cout << "Check to alphabet" << endl;
 		}
 	}
 	cout << "모음: " << vowel << endl;
 	cout << "자음: " << consonant << endl;
-
 }
