965935e9f1902e5e0fc81fed5d043aeec3093b63
수인 박 <psi7599@naver.com>
2022-09-14 00:55:13 +0900

    수정

--- Lab_2wk_Q2/Q2.cpp
+++ Lab_2wk_Q2/Q2.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
 int main() {
-	cout << "산수 문제를 자동으로 출제합니다." << endl;
 
+	random_device rd;
+	mt19937_64 mersenne(rd());
+	uniform_int_distribution<> dice(1, 9);
+
+	cout << dice(mersenne) << endl;
+
+	cout << "산수 문제를 자동으로 출제합니다." << endl;
+	
 
 
 }
